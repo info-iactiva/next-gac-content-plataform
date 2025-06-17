1b38a2a83523948074597fe8346e0f2e469af669
info-iactiva <info@iactiva.ai>
2025-06-16 18:51:33 -0600

    fix: coment all

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,19 +1,19 @@
-import { NextResponse } from 'next/server'
-import bcrypt from 'bcryptjs'
-import { connectToDatabase } from "@/lib/mongodb/mongodb";
-import {User}  from "@/models/user";
+// import { NextResponse } from 'next/server'
+// import bcrypt from 'bcryptjs'
+// import { connectToDatabase } from "@/lib/mongodb/mongodb";
+// import {User}  from "@/models/user";
 
-export async function POST(request: Request) {
-  await connectToDatabase()
-  const { email, password } = await request.json()
+// export async function POST(request: Request) {
+//   await connectToDatabase()
+//   const { email, password } = await request.json()
 
-  const existingUser = await User.findOne({ email })
-  if (existingUser) {
-    return NextResponse.json({ error: 'Usuario ya existe' }, { status: 400 })
-  }
+//   const existingUser = await User.findOne({ email })
+//   if (existingUser) {
+//     return NextResponse.json({ error: 'Usuario ya existe' }, { status: 400 })
+//   }
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const newUser = await User.create({ email, password: hashedPassword })
+//   const hashedPassword = await bcrypt.hash(password, 10)
+//   const newUser = await User.create({ email, password: hashedPassword })
 
-  return NextResponse.json({ message: 'Usuario registrado', user: { id: newUser._id, email: newUser.email } })
-}
+//   return NextResponse.json({ message: 'Usuario registrado', user: { id: newUser._id, email: newUser.email } })
+// }
