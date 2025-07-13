0b294604a2b5ccdb191fe8819ec6a31ea0d1b08a
info-iactiva <info@iactiva.ai>
2025-07-13 13:46:38 -0600

    feat: other button

--- src/components/paypal/paypalbutton.tsx
+++ src/components/paypal/paypalbutton.tsx
@@ -2,62 +2,70 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import {  toast } from 'sonner'
 import { useRouter } from "next/navigation";
-import { time } from "console";
 
 export default function Suscripcion({ planId,userId }) {
-  const router = useRouter();
+  const router = useRouter();      
 
   return (    
-      <PayPalScriptProvider 
-            options={{
-        "clientId": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+    <div>
+       <PayPalScriptProvider options={{
+        "clientId":process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+        "enable-funding": "card",
+        components: "buttons",
         vault: true,
-        intent: "subscription", 
-      }}
-    >
-      <PayPalButtons
-        style={{ layout: "vertical" }}
-        fundingSource="paypal"
-        createSubscription={(data, actions) => {
-            return actions.subscription.create({
-            plan_id: planId,
-            });
-          }}          
+        "data-sdk-integration-source": "developer-studio",
+        "data-page-type": "product-details",
+      }}        
+      >
+        <PayPalButtons                    
+            style={{
+            shape: "rect",
+            layout: "vertical",
+            color: "gold",
+            label: "paypal",
+          }}           
+          createSubscription={(data, actions) => actions.subscription.create({plan_id: planId })}
+
           onApprove={async (data, actions) => {
-          console.log("Suscripción creada:", data.subscriptionID);           
-          console.log(userId, planId);
-           try {
-            const res = await fetch("/api/paypal/save-subscription", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                subscriptionID: data.subscriptionID,
-                userId: parseInt(userId),
-                planId: planId,
-              }),
-            });
+            console.log("Suscripción creada:", data.subscriptionID);           
+            console.log(userId, planId);
 
-            if (!res.ok) {
-              const errorData = await res.json();
-              console.error("Error al guardar la suscripción:", errorData);
-              throw new Error(errorData.error || "Error al guardar la suscripción");
-            }
+            try {
+                const res = await fetch("/api/paypal/save-subscription", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    subscriptionID: data.subscriptionID,
+                    userId: parseInt(userId),
+                    planId: planId,
+                  }),
+                });
 
-            toast.success("Pago realizado con éxito");
+                if (!res.ok) {
+                  const errorData = await res.json();
+                  console.error("Error al guardar la suscripción:", errorData);
+                  throw new Error(errorData.error || "Error al guardar la suscripción");
+                }
 
-            setTimeout(() => {
-              router.push("/login"); 
-            }, 2000); // Redirige después de 2 segundos
+                toast.success("Pago realizado con éxito");
 
-          } catch (error: any) {
-            console.error("Error al guardar la suscripción:", error);
-            alert("Ocurrió un error: " + error.message);
-            toast.error("Error al guardar la suscripción");
-          }
-        }}
-      />
-    </PayPalScriptProvider>
+                setTimeout(() => {
+                  router.push("/login"); 
+                }, 2000); 
+
+              } catch (error: any) {
+                console.error("Error al guardar la suscripción:", error);
+                alert("Ocurrió un error: " + error.message);
+                toast.error("Error al guardar la suscripción");
+              }
+
+          }}
+        />        
+      </PayPalScriptProvider> 
+
+    </div>     
   );
 }
+
